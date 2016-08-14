632ebadf8488d277bee7872fa427faf11422ccea
vinay rayini <vinay rayini>
2016-08-13 20:23:13 -0500

    Setting up cloud deployment

--- app.js
+++ app.js
@@ -11,13 +11,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 
-//home
 app.get('/', routes.home);
 
 app.get('/star_wars_episode/:episode_number?', routes.movie_single);
 
 app.get('*', routes.notFound);
 
-app.listen(3000, function() {
-	console.log("Server is running!!");
-});
+app.listen(process.env.PORT || 3000);
+
+//For Local Deployment
+// app.listen(3000, function() {
+// 	console.log("Server is running!!");
+// });
